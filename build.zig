8c2970775db50ec33b9eb665bda28d3530380e84
Locria Cyber <74560659+iacore@users.noreply.github.com>
2023-07-30 16:15:05 +0000

    Update lmdb & build.zig

--- build.zig
+++ build.zig
@@ -1,20 +1,35 @@
 const std = @import("std");
-const deps = @import("deps.zig");
 
-const Builder = std.build.Builder;
-
-pub fn build(b: *Builder) void {
+pub fn build(b: *std.Build) void {
     var target = b.standardTargetOptions(.{});
     if (target.isGnuLibC()) target.abi = .musl;
 
-    const mode = b.standardReleaseOptions();
+    const optimize = b.standardOptimizeOption(.{});
 
-    const tests = b.addTestExe("test", "lmdb.zig");
-    tests.setTarget(target);
-    tests.setBuildMode(mode);
-    tests.install();
-    deps.addAllTo(tests);
+    const lib = b.addStaticLibrary(.{
+        .name = "lmdb",
+        .target = target,
+        .optimize = optimize,
+    });
+    lib.linkLibC();
+    lib.addCSourceFiles(&.{ "lmdb/libraries/liblmdb/mdb.c", "lmdb/libraries/liblmdb/midl.c" }, &.{});
+    // .c_source_flags = &.{ "-fno-sanitize=undefined" },
+
+    const pkg = b.addModule("lmdb", .{
+        .source_file = .{ .path = "lmdb.zig" },
+    });
+
+    const tests = b.addTest(.{
+        .name = "test",
+        .root_source_file = pkg.source_file,
+        .target = target,
+        .optimize = optimize,
+    });
+    tests.addIncludePath("lmdb/libraries/liblmdb");
+    tests.linkLibrary(lib);
+
+    b.installArtifact(tests);
 
     const test_step = b.step("test", "Run libary tests");
-    test_step.dependOn(&tests.run().step);
+    test_step.dependOn(&b.addRunArtifact(tests).step);
 }
